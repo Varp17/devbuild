37f7760ef3920ad7fe5cd129acb1ba3d6ca02f48
Varun Patil <varunpatil0217@gmail.com>
2025-01-12 21:05:54 +0530

    Adding CSS

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Placeholder for future JavaScript functionality
 console.log("Welcome to Devbuilds Solutions!");
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to DevBuilds!");
+    // Add custom JavaScript if needed for interactivity
+});
